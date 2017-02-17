1c35481daeac76c4a19b1631b837822955d9a615
spikeygriffiths <jonathan@rosepip.com>
2017-02-17 19:32:09 +0000

    Editing rules from web page now works!
    Renaming devices next...

---
+++ rules.php
@@ -0,0 +1,29 @@
+<?php
+
+echo "<center>";
+echo "<H1>Rules</H1>";
+echo "</center>";
+echo "<form action=\"save_rules.php\" method=\"post\">";
+ShowRules("/home/pi/hubapp/rules.txt");
+echo "<input type=\"submit\" value=\"Save New Rules\">";
+echo "</form>";
+echo "<center>";
+echo "Click <a href=\"index.php\">here</a> to return";
+echo "</center>";
+
+function ShowRules($filename)
+{
+    $index = 0;
+    $handle = fopen($filename, "r");
+    if ($handle) {
+        while (!feof($handle)) {
+            $line = fgets($handle);
+            echo "<input type=\"text\" size=\"100\" name=\"rule", $index, "\" value=\"", $line, "\"><br>";
+            $index++;
+        }
+        fclose($handle); 
+    }
+    echo "<input type=\"text\" size=\"100\" name=\"rule", $index, "\"><br>"; // Always have a blank rule at the end to add 
+    echo "<input type=\"hidden\" name=\"numRules\", value=\"",$index+1,"\">";
+}
+?>
---
+++ save_rules.php
@@ -0,0 +1,30 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+//shell_exec("chmod 666 /home/pi/hubapp/rules.txt");
+$numRules=$_POST["numRules"];
+$handle = fopen("/home/pi/hubapp/rules.txt", "w"); // or die("Can't save rules");
+if ($handle) {
+    $previousRule = "";
+    for ($index = 0; $index < $numRules; $index++) {
+        $name = "rule".$index;
+        $ruleLine = $_POST[$name];
+        $ignore = False;
+        if ($ruleLine == "") {
+            if ($previousRule == "") { // Lose any double blank lines
+                $ignore = True;
+            }
+        }
+       $previousRule = $ruleLine;
+        if ($ignore == False) {
+            $ruleLine = $ruleLine."\n";
+            fputs($handle, $ruleLine);
+        }
+    }
+    fclose($handle); 
+    echo "<meta http-equiv=\"refresh\" content=\"1;url=rules.php\" />";
+    echo "New rules saved";
+} else {
+    echo "<br>Try chmod 666 home/pi/hubapp/rules.txt...<br>";
+}
+?>
