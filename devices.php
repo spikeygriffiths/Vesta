5d39c33cf56dd6bc16ca6cbdb0b5c633e5dc39f9
Jonathan Griffiths <jonathan@rosepip.com>
2017-03-16 00:38:31 +0000

    More SQL in devices.php. Doesn't work yet,though

--- devices.php
+++ devices.php
@@ -17,8 +17,8 @@ echo "<center><h1>Devices</h1> ";
 echo "<button type=\"button\" onclick=\"window.location.href='AddNewDevices.php'\">Add new devices</button><br>";
 echo "</center>";
 echo "<form action=\"save_names.php\" method=\"post\">";
-$db = new SQLite3("hubstuff.db");
-ShowDevices($db);   //was ("/home/pi/hubapp/usernames.txt", "/home/pi/hubapp/status.xml");
+//$db = new SQLite3("/home/pi/hubapp/hubstuff.db");
+//ShowDevices($db);   //was ("/home/pi/hubapp/usernames.txt", "/home/pi/hubapp/status.xml");
 echo "<input type=\"submit\" value=\"Update Names\">";
 echo "</form>";
 echo "<center>";
@@ -26,32 +26,34 @@ echo "Click <a href=\"index.php\">here</a> to return";
 echo "</center>";
 echo "</body></html>";
 
+function TableElement($item, $units, $devIndex, $db)
+{
+    //$result = $db->query("SELECT * from events where item='".$item."' and devRowId=".$devIndex." order by timestamp desc limit 1");
+    //if ($result->numColumns() > 0) {
+    //    $val = $result->fetchArray();
+    //    echo "<td>",$val,$units,"</td>";
+    //} else 
+echo "<td>N/A</td>";
+}
+
 function ShowDevices($db)
 {
     $index = 0;
+    $result = $db->query("SELECT COUNT(*) FROM Devices");
+    $numDevs = $result->fetchArray();
+    echo "Number of devices: ",$numDevs;
     echo "<table>";
     echo "<tr><th>Name</th><th>Battery %</th><th>Temperature (C)</th><th>Presence</th><th>Notes</th></tr>";
-    $numDevs = $db->query("SELECT COUNT(*) FROM Devices");
     for ($devIndex = 0; $devIndex < $numDevs; $devIndex++) {
-        $username = $db->query("SELECT UserName from devices limit ".$index.",1");
+        $username = $db->query("SELECT UserName FROM devices LIMIT ".$index.",1");
         echo "<tr><td><input type=\"size\" text=\"20\" name=\"",$username, "\" value=\"", $line, "\"></td>";
-        // Status might be better as a direct socket query for "UserName=<blah>" which could return a nice list...
-        $battLvl = $db->query("SELECT * from events where item='Battery' and devRowId=".$devIndex." order by timestamp desc limit 1");
-        if ("N/A" != $battLvl) {
-            echo "<td>".$battLvl."%</td>";
-        } else {
-            echo "<td>".$battLvl."</td>";
-        }
-        $temp = $db->query("SELECT * from events where item='Temperature' and devRowId=".$devIndex." order by timestamp desc limit 1");
-        $temp = $xml->device[$index]->Temperature;
-        if ("N/A" != $temp) {
-            echo "<td>".$temp."'C</td>";
-        } else {
-            echo "<td>".$temp."</td>";
-        }
-        echo "<td>".$xml->device[$index]->Presence."</td>";
-        echo "<td>".$xml->device[$index]->Other."</td></tr>";
+	    TableElement("Battery", "%", $devIndex, $db);
+	    TableElement("Temperature", "'C", $devIndex, $db);
+	    TableElement("Presence", "", $devIndex, $db);
+	    TableElement("Other", "", $devIndex, $db);
     }
+    echo "</table>";
+    echo "<input type=\"hidden\" name=\"numNames\", value=\"",$numDevs,"\">";
 }
 
 function OldShowDevices($names, $status)
