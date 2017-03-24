771c3a6c70bafca340c524d62c06d177bd0680ff
spikeygriffiths <jonathan@rosepip.com>
2017-03-24 10:33:23 +0000

    activity log page now working

--- activity.php
+++ activity.php
@@ -1,7 +1,11 @@
 <?php 
 error_reporting(E_ALL); 
 
-echo "<html><body>";
+echo "<html>";
+echo "<head><style>table {font-family:arial, sans-serif;border-collapse: collapse;width: 100 % }";
+echo "td, th {border: 2px solid #dddddd;text-align: left;padding: 2px }";
+echo "</style></head>";
+echo "<body>";
 echo "<center><h1>Activity</h1> </center>";
 $dir = "sqlite:/home/pi/hubapp/hubstuff.db";
 $db = new PDO($dir) or die("Cannot open database");
@@ -9,14 +13,22 @@ ShowActivity($db);
 echo "<center><a href=\"index.php\">Home</a></center>";
 echo "</body></html>";
 
+function  DbGetItem($item, $devIdx, $db)
+{
+    $result = $db->query("SELECT ".$item." FROM Devices WHERE devIdx=".$devIdx);
+    $result->setFetchMode(PDO::FETCH_ASSOC);
+    $fetch = $result->fetch();
+    return $fetch[$item];
+}
+
 function ShowActivity($db)
 {
     $sth = $db->prepare("SELECT * FROM Events");
     $sth->execute();
     echo "<table>";
     while ($row =  $sth->fetch()) {
-        // Use $row['devIdx'] as key into Devices table to get $userName
-        echo"<tr><td>".$row['timestamp']."</td><td>".$row['item']."</td><td>".$row['value']."</td></tr>";
+        $username = DbGetItem("userName", $row['devIdx'],$db);        // Use $row['devIdx'] as key into Devices table to get $userName
+        echo"<tr><td>".$username."</td><td>".$row['timestamp']."</td><td>".$row['item']."</td><td>".$row['value']."</td></tr>";
     }
     echo "</table>";
 }
