800b47b688a543c044cca4c38e91d39730cc8ad3
spikeygriffiths <jonathan@rosepip.com>
2017-12-05 17:24:32 +0000

    Changed config back to case-sensitive after the insensitive version failed to find anything

--- config.py
+++ config.py
@@ -14,14 +14,14 @@ def Get(item, default=None):
             for line in config:
                 if line[0] != "#":  # Ignore comment lines
                     lineList = line.split("=") # Make each line into a list consisting of item on left side and value on right side of "="
-                    if lineList[0].strip().lower == item.lower: # Strip leading & trailing spaces and do a case-insensitive match
+                    if lineList[0].strip() == item: # Strip leading & trailing spaces and do a case-insensitive match
                         value = lineList[1].splitlines()[0] # Discard trailing newline
                         value = value.strip()   # Discard leading or trailing spaces
                         #log.debug("Matched:"+lineList[0]+ "with value:"+value)
                         return value
     if default != None:
-        log.debug("Missing variable from config.txt : " + item + "  Using default of " + default)
+        log.debug("Missing variable from " + configFilename + " : " + item + "  Using default of " + default)
     else:
-        log.debug("Missing variable from config.txt : " + item + "  No default")
+        log.debug("Missing variable from " + configFilename + " : " + item + "  No default")
     return default  # If we get all through the config file without finding a match, use default value
  
