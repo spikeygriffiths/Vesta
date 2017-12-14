531bc9fc27cd8dbd9ffe44993b5db0306f35011b
spikeygriffiths <jonathan@rosepip.com>
2017-12-14 17:25:18 +0000

    Fixed RequestStatus php page to use synopsis

--- RequestStatus.php
+++ RequestStatus.php
@@ -4,7 +4,7 @@ include "header.php";   # Has other includes as well as log-out detection, and f
 echo "</head><body>";
 ini_set('display_errors', '1');
 
-echo AppCmd("status", False);    # Build status page
-echo "<meta http-equiv=\"refresh\" content=\"0;url=/vesta/status.html\"/>";  # And then display it
+echo AppCmd("synopsis", False);    # Build status page
+echo "<meta http-equiv=\"refresh\" content=\"0;url=/vesta/synopsis.html\"/>";  # And then display it
 echo "</body></html>";
 ?>
