ca9d1cb84aedfdd5d462505f1cc6d692ca84f6e2
spikeygriffiths <jonathan@rosepip.com>
2017-12-19 17:17:16 +0000

    Database stats done for now

--- DbTableStats.php
+++ DbTableStats.php
@@ -18,20 +18,34 @@ echo "</body></html>";
 
 function ShowTableStats($db, $table)
 {
-    echo "<table>";
-    echo "<tr><th>Table</th><th>Entries</th></tr>";
-    ShowStat($db, $table);
-    echo "</table>";
+    $numDevs = GetDevCount($db);
+    if ($numDevs > 0) {
+        echo "<table>";
+        echo "<tr><th>Device</th><th>Last 24 hours</th><th>Last 7 days</th><th>Total</th></tr>";
+        for ($index = 0; $index < $numDevs; $index++) {
+            $devKey = GetDevKey($index, $db);
+            $username = GetDevItem("userName", $devKey, $db);
+            echo "<tr>";
+            echo "<td><a href=\"/vesta/ShowOneDevice.php/?devKey=",$devKey,"\">",$username,"</a></td>";
+            ShowStat($db, $table, "devKey=".$devKey." and timestamp>datetime('now', '-24 hours')");
+            ShowStat($db, $table, "devKey=".$devKey." and timestamp>datetime('now', '-7 days')");
+            ShowStat($db, $table, "devKey=".$devKey);
+            echo "</tr>";
+        }
+        echo "<tr>";
+        echo "<td><b>Totals</b></td>";
+        ShowStat($db, $table, "timestamp>datetime('now', '-24 hours')");
+        ShowStat($db, $table, "timestamp>datetime('now', '-7 days')");
+        ShowStat($db, $table);
+        echo "</tr>";
+        echo "</table>";
+    }
 }
 
-function ShowStat($db, $table)
+function ShowStat($db, $table, $cond)
 {
-    $entries = GetCount($db, $table);
-    if ($entries >= 100) {  # Don't bother showing small tables
-        echo "<tr>";
-        echo "<td><a href=\"/vesta/DbTableStats.php/?table=",$table,"\">",$table,"</a></td>";
-        echo "<td>",$entries,"</td></tr>";
-    }
+    $entries = GetCount($db, $table, $cond);
+    echo "<td>",$entries,"</td>";
 }
 
 ?>
