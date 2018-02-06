ebfe190044befe43acb5f3c9ba2affe7f76298e9
spikeygriffiths <jonathan@rosepip.com>
2018-02-06 17:29:16 +0000

    Trying (and failing) to align form in Config.php

--- Config.php
+++ Config.php
@@ -3,7 +3,6 @@ include "database.php";
 include "functions.php";
 include "header.php";   # Has other includes as well as log-out detection, and favicon.  NB Has "<html><head>" for favicon link!
 echo "</head><body>";
-echo "<center>";
 PageHeader("Configuration");
 $db = DatabaseInit();
 ShowConfig($db);
@@ -14,15 +13,18 @@ function ShowConfig($db)
 {
     $sth = $db->prepare("SELECT * FROM Config");
     $sth->execute();
+    echo "<div class=\"configForm\">";
     while ($row =  $sth->fetch()) {
         $item = $row['item'];
         $value = $row['value'];
-        echo "<form action=\"/vesta/save_config.php/?item=", $item, "\" method=\"post\">$item: ";
-        echo "<input type=\"text\" size=\"80\" name=\"valueTxt\" value=\"", $value, "\">";
+        //echo "<form align=\"right\" action=\"/vesta/save_config.php/?item=", $item, "\" method=\"post\">",$item,": "; // I can't get form to be right-aligned inside <div> element
+        echo "<form action=\"/vesta/save_config.php/?item=", $item, "\" method=\"post\">",$item,": ";
+        echo "<input type=\"text\" size=\"50\" name=\"valueTxt\" value=\"", $value, "\">";
         echo "<input type=\"submit\" value=\"Update\"></form>";
     }
     echo "<form action=\"/vesta/save_config.php/?item=NewItem\" method=\"post\">New item: ";
-    echo "<input type=\"text\" size=\"30\" name=\"valueTxt\" value=\"\"\>";
+    echo "<input type=\"text\" size=\"20\" name=\"valueTxt\" value=\"\"\>";
     echo "<input type=\"submit\" value=\"Update\"></form>";
+    echo "</div>";
 }
 ?>
