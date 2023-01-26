ed501412ffb44504767c929f90e11d591cde3223
spikeygriffiths <jonathan@rosepip.com>
2023-01-26 15:41:26 +0000

    Fix AppLog to use logs dir

--- AppLog.php
+++ AppLog.php
@@ -8,7 +8,7 @@ echo "</head><body>";
 PageHeader("Application log now");
 
 echo "</center>";
-$logHandle = fopen("/home/pi/Vesta/today.log", "r");
+$logHandle = fopen("/home/pi/Vesta/logs/today.log", "r");
 fseek($logHandle, -4000, SEEK_END); // Back 4000 chars from the end (assume ~50 lines of 80 chars)
 $logText = fread($logHandle, 4000);
 echo nl2br($logText);
