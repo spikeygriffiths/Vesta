5ee1abb6154b21d65f62ef1f2397e8e377015cdc
spikeygriffiths <jonathan@rosepip.com>
2023-12-28 18:17:00 +0000

    vestamon now functional

--- vestamon.py
+++ vestamon.py
@@ -1,19 +1,22 @@
 import subprocess
 import database
 import sendmail
+import time
 
-result = subprocess.run(['ps', 'ax'], stdout=subprocess.PIPE)
-res = result.stdout.decode().splitlines()
-vesta_running = False
-for item in res:
-    if "vesta.py" in item:
-        vesta_running = True
-if vesta_running:
-    print("Vesta is running")
-else:
-    print("Vesta stopped!")
+def check(app_name):
+    result = subprocess.run(['ps', 'ax'], stdout=subprocess.PIPE)
+    res = result.stdout.decode().splitlines()
+    app_running = False
+    for item in res:
+        if app_name in item:
+            app_running = True
+    if not app_running:
+        database.ConnectDb()
+        result = subprocess.run(['tail', 'logs/today.log', "-n", "20"], stdout=subprocess.PIPE)
+        text = result.stdout.decode().splitlines()
+        sendmail.email(app_name + " stopped", text, "")
+        subprocess.run(["sudo", "/bin/vesta.sh"]) # Restart app
 
-database.ConnectDb()
-result = subprocess.run(['tail', 'logs/today.log'], stdout=subprocess.PIPE)
-text = result.stdout.decode().splitlines()
-sendmail.email("VestaLog", text, "")
+while True:
+    check("vesta.py")
+    time.sleep(60)
